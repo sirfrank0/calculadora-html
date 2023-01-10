25007e414043be6d960bb258771e635903a12e48
SirFrank0 <frankseb04@gmail.com>
2023-01-10 18:58:14 -0500

    parseFloat & switch

--- index.js
+++ index.js
@@ -8,10 +8,28 @@ function calcular(){
 
         //console.log(txtOperador.value)              //Con .value se muestra el valor dado para txtOperador
 
-    const operador = txtOperador.value
+    const operador = txtOperador.value;
+    const op1 = parseFloat(txtOp1.value) ;      //parseFloat para convertir string en numeros decimales
+    const op2 = parseFloat(txtOp2.value) ;
 
     if(operador == "+" || operador == "-" || operador == "*" || operador == "/" ){          // if para saber si un input es posible o no 
-        pResultado.innerText = 'calculo posible'
+        let resultado;
+        switch (operador){                      // swicth para reemplzar esle if y crear varias situaciones
+
+            case "+": resultado= op1 + op2
+            break;
+
+            case "-": resultado= op1 - op2
+            break;
+
+            case "*": resultado= op1 * op2
+            break;
+
+            case "/": resultado= op1 / op2
+            break;
+
+        }
+        pResultado.innerText = 'calculo posible'                                            // Con innerText se aniade texto a una etiqueta de HTML
     }else{
         pResultado.innerText = 'calculo imposible'
     }

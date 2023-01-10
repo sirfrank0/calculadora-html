50dd99943f27860e5394fb87c6a728cef8f221ff
SirFrank0 <frankseb04@gmail.com>
2023-01-10 18:36:00 -0500

    first steps on jS file

---
+++ index.js
@@ -0,0 +1,9 @@
+const txtOp1 = document.getElementById('op1');
+const txtOperador = document.getElementById('operador');
+const txtOp2 = document.getElementById('op2');
+const btnCalcular = document.getElementById('Calcular');
+const pResultado = document.getElementById('resultado')
+
+function calcular(){
+    console.log('Apretaste el boton calcular: ')
+}

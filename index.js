4c9cd5570bd2afefc2cbb754253da9ca9881a573
SirFrank0 <frankseb04@gmail.com>
2023-01-10 18:45:06 -0500

    if operador || posible

--- index.js
+++ index.js
@@ -5,5 +5,16 @@ const btnCalcular = document.getElementById('Calcular');
 const pResultado = document.getElementById('resultado')
 
 function calcular(){
-    console.log(txtOperador.value)              //Con .value se muestra el valor dado para txtOperador
+
+        //console.log(txtOperador.value)              //Con .value se muestra el valor dado para txtOperador
+
+    const operador = txtOperador.value
+
+    if(operador == "+" || operador == "-" || operador == "*" || operador == "/" ){
+        console.log('calculo posible')
+    }else{
+        console.log('calculo imposible')
+    }
+
+
 }
